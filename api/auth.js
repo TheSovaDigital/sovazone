e1326be7bc213a75e4caa42e9aef24738378fe25
Sovaigital <thesovaz@gmail.com>
2026-04-06 20:36:13 +0400

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,10 +1,9 @@
 export default async function handler(req, res) {
   const client_id = process.env.GITHUB_CLIENT_ID;
-  const host = req.headers.host;
-  const redirect_uri = `https://${host}/api/callback`;
+  const redirect_uri = "https://sovazone.vercel.app/api/callback";
 
   const url =
-    `https://github.com/login/oauth/authorize` +
+    "https://github.com/login/oauth/authorize" +
     `?client_id=${client_id}` +
     `&scope=repo` +
     `&redirect_uri=${encodeURIComponent(redirect_uri)}`;
