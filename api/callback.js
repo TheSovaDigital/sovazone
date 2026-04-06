172c4a484e43d980842efacbd0f7862049100b55
Sovaigital <thesovaz@gmail.com>
2026-04-06 20:29:07 +0400

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,5 +1,7 @@
 export default async function handler(req, res) {
   const code = req.query.code;
+  const host = req.headers.host;
+  const redirect_uri = `https://${host}/api/callback`;
 
   const response = await fetch(
     "https://github.com/login/oauth/access_token",
@@ -13,13 +15,27 @@ export default async function handler(req, res) {
         client_id: process.env.GITHUB_CLIENT_ID,
         client_secret: process.env.GITHUB_CLIENT_SECRET,
         code,
+        redirect_uri,
       }),
     }
   );
 
   const data = await response.json();
 
-  const token = data.access_token;
+  if (!data.access_token) {
+    res.status(500).send(JSON.stringify(data));
+    return;
+  }
 
-  res.redirect(`/admin/#access_token=${token}&token_type=bearer`);
+  res.setHeader("Content-Type", "text/html");
+  res.send(`
+    <script>
+      window.opener.postMessage(
+        'authorization:github:success:' +
+        JSON.stringify({ token: '${data.access_token}', provider: 'github' }),
+        '*'
+      );
+      window.close();
+    </script>
+  `);
 }
