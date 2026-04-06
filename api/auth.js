0deab4a29342c75dbcda87962b66cba13dffd5d9
Sovaigital <thesovaz@gmail.com>
2026-04-06 20:27:50 +0400

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,12 +1,13 @@
 export default async function handler(req, res) {
   const client_id = process.env.GITHUB_CLIENT_ID;
-  const redirect_uri = `${req.headers.origin}/api/callback`;
+  const host = req.headers.host;
+  const redirect_uri = `https://${host}/api/callback`;
 
   const url =
     `https://github.com/login/oauth/authorize` +
     `?client_id=${client_id}` +
     `&scope=repo` +
-    `&redirect_uri=${redirect_uri}`;
+    `&redirect_uri=${encodeURIComponent(redirect_uri)}`;
 
   res.redirect(url);
 }
