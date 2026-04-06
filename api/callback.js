c56080898220d04052db0fd55d1243d99e5e1f6b
Sovaigital <thesovaz@gmail.com>
2026-04-06 21:53:04 +0400

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,6 +1,10 @@
 export default async function handler(req, res) {
   const code = req.query.code;
-  const redirect_uri = "https://sovazone.vercel.app/api/callback";
+
+  if (!code) {
+    res.status(400).send("Missing code");
+    return;
+  }
 
   const response = await fetch("https://github.com/login/oauth/access_token", {
     method: "POST",
@@ -12,26 +16,44 @@ export default async function handler(req, res) {
       client_id: process.env.GITHUB_CLIENT_ID,
       client_secret: process.env.GITHUB_CLIENT_SECRET,
       code,
-      redirect_uri,
+      redirect_uri: "https://sovazone.vercel.app/api/callback",
     }),
   });
 
   const data = await response.json();
 
   if (!data.access_token) {
-    res.status(500).send(JSON.stringify(data));
+    res.status(500).send(`
+      <pre>${JSON.stringify(data, null, 2)}</pre>
+    `);
     return;
   }
 
-  res.setHeader("Content-Type", "text/html");
+  const payload = {
+    token: data.access_token,
+    provider: "github",
+  };
+
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
   res.send(`
-    <script>
-      window.opener.postMessage(
-        'authorization:github:success:' +
-        JSON.stringify({ token: '${data.access_token}', provider: 'github' }),
-        '*'
-      );
-      window.close();
-    </script>
+    <!doctype html>
+    <html>
+      <body>
+        <script>
+          (function() {
+            const payload = ${JSON.stringify(payload)};
+            if (window.opener) {
+              window.opener.postMessage(
+                "authorization:github:success:" + JSON.stringify(payload),
+                window.location.origin
+              );
+              window.close();
+            } else {
+              document.body.innerHTML = "<pre>OAuth success, but no opener window found.</pre>";
+            }
+          })();
+        </script>
+      </body>
+    </html>
   `);
 }
